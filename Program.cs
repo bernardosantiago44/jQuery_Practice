6d79cb0245721402b25c39959c04197ded320a07
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-06-16 19:10:58 -0600

    Project setup

---
+++ Program.cs
@@ -0,0 +1,45 @@
+
+using Microsoft.Data.SqlClient;
+using Serilog;
+
+namespace jQuery_Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+            Log.Information("jQuery Practice - Bernardo Santiago");
+
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddTransient<SqlConnection>(_ => 
+                new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
